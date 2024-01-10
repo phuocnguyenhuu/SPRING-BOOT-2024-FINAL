b8a831d11c20ee89af4ec5a6e6e7d3c315fe4525
Phuoc Nguyen Huu <phuocnhhcmup@outlook.com>
2024-01-10 14:52:13 +0700

    UPDATE STUDENT

---
+++ 03-spring-boot-hibernate-jpa-crud/05-cruddemo-student-update/src/main/java/com/phuocnguyenhuu/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,20 @@
+package com.phuocnguyenhuu.cruddemo.dao;
+
+import com.phuocnguyenhuu.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+
+    void update(Student theStudent);
+
+}
---
+++ 03-spring-boot-hibernate-jpa-crud/05-cruddemo-student-update/src/test/java/com/phuocnguyenhuu/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuocnguyenhuu.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
