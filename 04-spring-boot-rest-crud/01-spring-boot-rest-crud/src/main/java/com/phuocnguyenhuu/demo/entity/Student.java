9c8b022e92b147d7959f12b4594c12ddb46beac7
Phuoc Nguyen Huu <phuocnhhcmup@outlook.com>
2024-01-16 22:58:30 +0700

    100. Spring Boot REST POJO - Coding - Part 1

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/phuocnguyenhuu/demo/entity/Student.java
@@ -0,0 +1,17 @@
+package com.phuocnguyenhuu.demo.entity;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	
+	// Constructor
+	public Student(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	
+	
+	
+}
---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/test/java/com/phuocnguyenhuu/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuocnguyenhuu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
