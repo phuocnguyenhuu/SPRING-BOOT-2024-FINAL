b2a4ee08b78e151bf802116c8ceae93eb430b687
Phuoc Nguyen Huu <phuocnhhcmup@outlook.com>
2024-01-18 23:01:51 +0700

    100. Spring Boot REST POJO - Coding - Part 1

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/phuocnguyenhuu/demo/entity/Student.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/phuocnguyenhuu/demo/entity/Student.java
@@ -11,6 +11,24 @@ public class Student {
 		this.lastName = lastName;
 	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+
 	
 	
 	
